d0aa1e40fd87b9f2ccdd3bffeea8fa52d82ec264
Sanjeev <thedevelopersanjeev@gmail.com>
2018-09-30 17:44:51 +0530

    removed extra code

--- app/src/main/java/com/developersanjeev/calculator/MainActivity.kt
+++ app/src/main/java/com/developersanjeev/calculator/MainActivity.kt
@@ -43,165 +43,113 @@ class MainActivity : AppCompatActivity() {
             R.id.button_one -> {
                 // get current string from display
                 val str:String = display_view.text.toString()
-                if(str == "0"){
-                    // display 1 instead of 01
-                    display_view.text = "1"
-                }
-                else {
-                    display_view.text = str + "1"
-                }
+                // display 1 instead of 01
+                if(str == "0") display_view.text = "1"
+                else display_view.text = str + "1"
             }
 
             // Button two is clicked
             R.id.button_two -> {
                 val str:String = display_view.text.toString()
-                if(str == "0"){
-                    display_view.text = "2"
-                }
-                else {
-                    display_view.text = str + "2"
-                }
+                if(str == "0") display_view.text = "2"
+                else display_view.text = str + "2"
             }
 
             // Button three is clicked
             R.id.button_three -> {
                 val str:String = display_view.text.toString()
-                if(str == "0"){
-                    display_view.text = "3"
-                }
-                else {
-                    display_view.text = str + "3"
-                }
+                if(str == "0") display_view.text = "3"
+                else display_view.text = str + "3"
             }
 
             // Button four is clicked
             R.id.button_four -> {
                 val str:String = display_view.text.toString()
-                if(str == "0"){
-                    display_view.text = "4"
-                }
-                else {
-                    display_view.text = str + "4"
-                }
+                if(str == "0") display_view.text = "4"
+                else display_view.text = str + "4"
             }
 
             // Button five is clicked
             R.id.button_five -> {
                 val str:String = display_view.text.toString()
-                if(str == "0"){
-                    display_view.text = "5"
-                }
-                else {
-                    display_view.text = str + "5"
-                }
+                if(str == "0") display_view.text = "5"
+                else display_view.text = str + "5"
             }
 
             // Button six is clicked
             R.id.button_six -> {
                 val str:String = display_view.text.toString()
-                if(str == "0"){
-                    display_view.text = "6"
-                }
-                else {
-                    display_view.text = str + "6"
-                }
+                if(str == "0") display_view.text = "6"
+                else display_view.text = str + "6"
             }
 
             // Button seven is clicked
             R.id.button_seven -> {
                 val str:String = display_view.text.toString()
-                if(str == "0"){
-                    display_view.text = "7"
-                }
-                else {
-                    display_view.text = str + "7"
-                }
+                if(str == "0") display_view.text = "7"
+                else display_view.text = str + "7"
             }
 
             // Button eight is clicked
             R.id.button_eight -> {
                 val str:String = display_view.text.toString()
-                if(str == "0"){
-                    display_view.text = "8"
-                }
-                else {
-                    display_view.text = str + "8"
-                }
+                if(str == "0") display_view.text = "8"
+                else display_view.text = str + "8"
             }
 
             // Button nine is clicked
             R.id.button_nine -> {
                 val str:String = display_view.text.toString()
-                if(str == "0"){
-                    display_view.text = "9"
-                }
-                else {
-                    display_view.text = str + "9"
-                }
+                if(str == "0") display_view.text = "9"
+                else display_view.text = str + "9"
             }
 
             // Button zero is clicked
             R.id.button_zero -> {
                 val str:String = display_view.text.toString()
-                if(str == "0"){
-                    display_view.text = "0"
-                }
-                else {
-                    display_view.text = str + "0"
-                }
+                if(str == "0") display_view.text = "0"
+                else display_view.text = str + "0"
             }
 
             // Button clear is clicked
-            R.id.button_clear -> {
-                display_view.text = "0"
-            }
+            R.id.button_clear -> display_view.text = "0"
 
             // Button plus is clicked
             R.id.button_plus -> {
                 val str:String = display_view.text.toString()
                 // checks if there is already an operator in the end
-                if(ExpressionEvaluator.checkValidity(str)){
-                    display_view.text = str + "+"
-                }
+                if(ExpressionEvaluator.checkValidity(str)) display_view.text = str + "+"
             }
 
             // Button minus is clicked
             R.id.button_minus -> {
                 val str:String = display_view.text.toString()
-                if(ExpressionEvaluator.checkValidity(str)){
-                    display_view.text = str + "-"
-                }
+                if(ExpressionEvaluator.checkValidity(str)) display_view.text = str + "-"
             }
 
             // Button multiply is clicked
             R.id.button_multiply -> {
                 val str:String = display_view.text.toString()
-                if(ExpressionEvaluator.checkValidity(str)){
-                    display_view.text = str + "x"
-                }
+                if(ExpressionEvaluator.checkValidity(str)) display_view.text = str + "x"
             }
 
             // Button divide is clicked
             R.id.button_divide -> {
                 val str:String = display_view.text.toString()
-                if(ExpressionEvaluator.checkValidity(str)){
-                    display_view.text = str + "/"
-                }
+                if(ExpressionEvaluator.checkValidity(str)) display_view.text = str + "/"
             }
 
             // Button change sign is clicked
             R.id.button_change_sign -> {
                 val str:String = display_view.text.toString()
-                if(ExpressionEvaluator.checkValidity(str)){
-                    try {
-                        // evaluate the current expression
-                        // multiply it by (-1) to change the sign
-                        val x = Integer.parseInt(ExpressionEvaluator.evaluateExpression(str).toString())
-                        display_view.text = (-1*x).toString()
-                    }
-                    catch (error:EvalError){
-                        // do nothing as expression can't be evaluated
-                    }
+                if(ExpressionEvaluator.checkValidity(str)) try {
+                    // evaluate the current expression
+                    // multiply it by (-1) to change the sign
+                    val x = Integer.parseInt(ExpressionEvaluator.evaluateExpression(str).toString())
+                    display_view.text = (-1*x).toString()
+                }
+                catch (error:EvalError){
+                    // do nothing as expression can't be evaluated
                 }
             }
 
@@ -211,27 +159,23 @@ class MainActivity : AppCompatActivity() {
                 // if condition to check the validity of expression
                 // as dot can't come after '+', '-', 'x', '/' or '.'
                 if(!(str.endsWith("+") || str.endsWith("-")
-                                || str.endsWith("x") || str.endsWith("/") || str.endsWith("."))){
-                    display_view.text = str + "."
-                }
+                                || str.endsWith("x") || str.endsWith("/") || str.endsWith("."))) display_view.text = str + "."
             }
 
             // Button percentage is clicked
             R.id.button_percentage -> {
                 val str:String = display_view.text.toString()
                 // first check the validity of expression
-                if(ExpressionEvaluator.checkValidity(str)){
-                    try{
-                        // evaluate the current expression
-                        // divide it by 100.0 to find percent
-                        // again evaluate it
-                        display_view.text = ExpressionEvaluator.evaluateExpression(
-                                ExpressionEvaluator.evaluateExpression(str).toString() + "/100.0")
-                                .toString()
-                    }
-                    catch(error:EvalError){
-                        display_view.text = "INVALID"
-                    }
+                if(ExpressionEvaluator.checkValidity(str)) try{
+                    // evaluate the current expression
+                    // divide it by 100.0 to find percent
+                    // again evaluate it
+                    display_view.text = ExpressionEvaluator.evaluateExpression(
+                            ExpressionEvaluator.evaluateExpression(str).toString() + "/100.0")
+                            .toString()
+                }
+                catch(error:EvalError){
+                    display_view.text = "INVALID"
                 }
             }
 
@@ -239,13 +183,11 @@ class MainActivity : AppCompatActivity() {
             R.id.button_equals -> {
                 val str:String = display_view.text.toString()
                 // Simply check the validity and evaluate the expression
-                if(ExpressionEvaluator.checkValidity(str)){
-                    try {
-                        display_view.text = ExpressionEvaluator.evaluateExpression(str).toString()
-                    }
-                    catch(error:EvalError){
-                        display_view.text = "INVALID"
-                    }
+                if(ExpressionEvaluator.checkValidity(str)) try {
+                    display_view.text = ExpressionEvaluator.evaluateExpression(str).toString()
+                }
+                catch(error:EvalError){
+                    display_view.text = "INVALID"
                 }
             }
         }
